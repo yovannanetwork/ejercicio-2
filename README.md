a44dd93293c599304144d5f418daaed1ed48df68
yovannanetwork <yovannanetwork@gmail.com>
2016-12-20 09:27:56 -0600

    java script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ejercicio-2
-promedio de tres numeros
+inicio: promedio de tres numeros
+
+# ingreso de datos
+
+- num1
+- num2
+- num3
+- sumar
+- promedio
+# proceso
+- pedir num1
+- pedir num2
+- pedir num3
+- sumar (num1, num2 y num3)
+- prodediar sumar/3
+
+# fin
+
---
+++ main.js
@@ -0,0 +1,10 @@
+var num1, num2, num3, sumar, promedio;
+
+num1 = prompt("por favor, ingrese num1");
+num2 = prompt("por favor, ingrese num2");
+num3 = prompt("por favor, ingrese num3");
+
+sumar = parseInt(num1) + parseInt(num2) + parseInt(num3);
+promedio = sumar / 3;
+
+alert(promedio);
